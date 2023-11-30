2744f5ce226d7934999df823ff16350b8fcde426
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2023-12-01 00:28:03 +0800

    Supported custom symbol name

--- Sources/SFSymbol/SFSymbol.swift
+++ Sources/SFSymbol/SFSymbol.swift
@@ -1,72 +1,99 @@
 #if canImport(AppKit) && !targetEnvironment(macCatalyst)
 
-    import AppKit
+import AppKit
 
-    extension SFSymbol {
-        public typealias CocoaImage = NSImage
-        public typealias CocoaFont = NSFont
-        public typealias CocoaColor = NSColor
-        public typealias SymbolConfiguration = NSImage.SymbolConfiguration
-        public typealias SymbolWeight = NSFont.Weight
-        public typealias SymbolScale = NSImage.SymbolScale
-        public typealias SymbolTextStyle = NSFont.TextStyle
-    }
+extension SFSymbol {
+    public typealias CocoaImage = NSImage
+    public typealias CocoaFont = NSFont
+    public typealias CocoaColor = NSColor
+    public typealias SymbolConfiguration = NSImage.SymbolConfiguration
+    public typealias SymbolWeight = NSFont.Weight
+    public typealias SymbolScale = NSImage.SymbolScale
+    public typealias SymbolTextStyle = NSFont.TextStyle
+}
 
 #elseif canImport(UIKit)
 
-    import UIKit
+import UIKit
 
-    extension SFSymbol {
-        public typealias CocoaImage = UIImage
-        public typealias CocoaFont = UIFont
-        public typealias CocoaColor = UIColor
-        public typealias SymbolConfiguration = UIImage.SymbolConfiguration
-        public typealias SymbolWeight = UIImage.SymbolWeight
-        public typealias SymbolScale = UIImage.SymbolScale
-        public typealias SymbolTextStyle = UIFont.TextStyle
-    }
+extension SFSymbol {
+    public typealias CocoaImage = UIImage
+    public typealias CocoaFont = UIFont
+    public typealias CocoaColor = UIColor
+    public typealias SymbolConfiguration = UIImage.SymbolConfiguration
+    public typealias SymbolWeight = UIImage.SymbolWeight
+    public typealias SymbolScale = UIImage.SymbolScale
+    public typealias SymbolTextStyle = UIFont.TextStyle
+}
 #else
 
-    #error("Unsupported Platform")
+#error("Unsupported Platform")
 
 #endif
 
 import SwiftUI
 
 public final class SFSymbol {
-    public let name: Name
+    public let name: SymbolName
 
     public private(set) var configuration: SymbolConfiguration?
 
     public private(set) var variableValue: Double?
 
-    public init(name: Name) {
+    public init(name: SymbolName) {
         self.name = name
     }
 
-    public init(name: Name, pointSize: CGFloat, weight: SymbolWeight) {
-        self.name = name
-        self.configuration = .init(pointSize: pointSize, weight: weight)
+    public convenience init(name: SymbolName, pointSize: CGFloat, weight: SymbolWeight) {
+        self.init(name: name, pointSize: pointSize, weight: weight, scale: nil)
     }
 
-    public init(name: Name, pointSize: CGFloat, weight: SymbolWeight, scale: SymbolScale) {
-        self.name = name
-        self.configuration = .init(pointSize: pointSize, weight: weight, scale: scale)
+    public convenience init(name: SymbolName, pointSize: CGFloat, weight: SymbolWeight, scale: SymbolScale?) {
+        self.init(name: name)
+        if let scale {
+            self.configuration = .init(pointSize: pointSize, weight: weight, scale: scale)
+        } else {
+            self.configuration = .init(pointSize: pointSize, weight: weight)
+        }
     }
 
-    public init(name: Name, textStyle: SymbolTextStyle) {
-        self.name = name
-        self.configuration = .init(textStyle: textStyle)
+    public convenience init(name: SymbolName, textStyle: SymbolTextStyle) {
+        self.init(name: name, textStyle: textStyle, scale: nil)
     }
 
-    public init(name: Name, textStyle: SymbolTextStyle, scale: SymbolScale) {
-        self.name = name
-        self.configuration = .init(textStyle: textStyle, scale: scale)
+    public convenience init(name: SymbolName, scale: SymbolScale) {
+        self.init(name: name, textStyle: nil, scale: scale)
     }
 
-    public init(name: Name, scale: SymbolScale) {
-        self.name = name
-        self.configuration = .init(scale: scale)
+    public convenience init(name: SymbolName, textStyle: SymbolTextStyle?, scale: SymbolScale?) {
+        self.init(name: name)
+        if let textStyle, let scale {
+            self.configuration = .init(textStyle: textStyle, scale: scale)
+        } else if let textStyle {
+            self.configuration = .init(textStyle: textStyle)
+        } else if let scale {
+            self.configuration = .init(scale: scale)
+        }
+    }
+
+    public convenience init(name: SystemSymbolName, pointSize: CGFloat, weight: SymbolWeight) {
+        self.init(name: name, pointSize: pointSize, weight: weight, scale: nil)
+    }
+
+    public convenience init(name: SystemSymbolName, pointSize: CGFloat, weight: SymbolWeight, scale: SymbolScale?) {
+        self.init(name: name as SymbolName, pointSize: pointSize, weight: weight, scale: scale)
+    }
+
+    public convenience init(name: SystemSymbolName, textStyle: SymbolTextStyle) {
+        self.init(name: name, textStyle: textStyle, scale: nil)
+    }
+
+    public convenience init(name: SystemSymbolName, scale: SymbolScale) {
+        self.init(name: name, textStyle: nil, scale: scale)
+    }
+
+    public convenience init(name: SystemSymbolName, textStyle: SymbolTextStyle?, scale: SymbolScale?) {
+        self.init(name: name as SymbolName, textStyle: textStyle, scale: scale)
     }
 
     @available(macOS 12.0, *)
@@ -97,14 +124,14 @@ public final class SFSymbol {
         return self
     }
 
-    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0,  *)
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
     public func hierarchicalColor(_ color: CocoaColor) -> Self {
         let otherConfiguration = SymbolConfiguration(hierarchicalColor: color)
         configuration = configuration.map { $0.applying(otherConfiguration) } ?? otherConfiguration
         return self
     }
 
-    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0,  *)
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
     public func paletteColors(_ colors: [CocoaColor]) -> Self {
         let otherConfiguration = SymbolConfiguration(paletteColors: colors)
         configuration = configuration.map { $0.applying(otherConfiguration) } ?? otherConfiguration
@@ -125,42 +152,60 @@ public final class SFSymbol {
     }
 
     #if canImport(AppKit) && !targetEnvironment(macCatalyst)
-        public var nsImage: NSImage {
-            var image = if let variableValue, #available(macOS 13.0, *) {
+    public var nsImage: NSImage {
+        var image = if let variableValue, #available(macOS 13.0, *) {
+            if name is SystemSymbolName {
                 NSImage(systemSymbolName: name.rawValue, variableValue: variableValue, accessibilityDescription: nil)
             } else {
+                NSImage(symbolName: name.rawValue, bundle: name.bundle, variableValue: variableValue)
+            }
+        } else {
+            if name is SystemSymbolName {
                 NSImage(systemSymbolName: name.rawValue, accessibilityDescription: nil)
+            } else if let bundle = name.bundle {
+                bundle.image(forResource: name.rawValue)
+            } else {
+                NSImage(named: name.rawValue)
             }
-
-            if let configuration {
-                image = image?.withSymbolConfiguration(configuration)
-            }
-
-            guard let image else {
-                fatalError("Internal Error, check the Name enum for spelling errors")
-            }
-            
-            return image
         }
+
+        if let configuration {
+            image = image?.withSymbolConfiguration(configuration)
+        }
+
+        guard let image else {
+            fatalError("Internal Error, check the Name enum for spelling errors")
+        }
+
+        return image
+    }
 
     #elseif canImport(UIKit)
 
-        public var uiImage: UIImage {
-            let image = if let variableValue, #available(iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
+    public var uiImage: UIImage {
+        let image = if let variableValue, #available(iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
+            if name is SystemSymbolName {
                 UIImage(systemName: name.rawValue, variableValue: variableValue, configuration: configuration)
             } else {
+                UIImage(named: name.rawValue, in: name.bundle, variableValue: variableValue, configuration: configuration)
+            }
+        } else {
+            if name is SystemSymbolName {
                 UIImage(systemName: name.rawValue, withConfiguration: configuration)
+            } else {
+                UIImage(named: name.rawValue, in: name.bundle, with: configuration)
             }
-            
-            guard let image else {
-                fatalError("Internal Error, check the Name enum for spelling errors")
-            }
-            
-            return image
         }
 
+        guard let image else {
+            fatalError("Internal Error, check the Name enum for spelling errors")
+        }
+
+        return image
+    }
+
     #endif
-    
+
     public var image: SwiftUI.Image {
         #if canImport(AppKit) && !targetEnvironment(macCatalyst)
         return .init(nsImage: nsImage)
@@ -170,5 +215,4 @@ public final class SFSymbol {
         #error("Unsupported Platform")
         #endif
     }
-    
 }
